54428b544a65b45983df9042e258b2ca689f9c21
Christopher Galdi <42784609+christophergaldi6@users.noreply.github.com>
2021-09-21 02:54:07 -0700

    First Project

---
+++ FirstProject Files/Assets/Scripts/DataLogger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Need access to write to txt file
+using System.IO;
+//For accessing Database/Resources
+using UnityEditor;
+
+public class DataLogger : MonoBehaviour
+{
+    private string fileName = "testLog";
+    private string path = "Assets/Logs/";
+    private string filePath;
+
+    // Use this for initialization
+    void Start()
+    {
+        WriteString("Startup");
+    }
+
+    public void WriteString(string message)
+    {
+        filePath = path + fileName + ".txt";
+
+        StreamWriter writer = new StreamWriter(filePath, true);
+
+        writer.WriteLine(message);
+        writer.WriteLine("TIMESTAMP: " + System.DateTime.Now);
+        writer.WriteLine("////////////////////////////////");
+
+        writer.Close();
+
+        AssetDatabase.ImportAsset(filePath);
+        Resources.Load(filePath);
+    }
+
+    void OnApplicationQuit() {
+        WriteString("Shutdown");
+    }
+}
---
+++ FirstProject Files/Assets/Scripts/test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class test : MonoBehaviour {
+    //Create a variable to link our existing DataLogger.cs in the scene 
+    private DataLogger dataLoggerReference;
+
+    void Start()
+    {
+        //have unity find your gameobject with this taf and assign this component as the "dataLoggerReference" variable 
+        dataLoggerReference = GameObject.FindGameObjectWithTag("DataLogger").GetComponent<DataLogger>();
+    }
+    // Update is called once per frame
+    void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dataLoggerReference.WriteString("spacebar was pressed");
+        }
+	}
+    public void ButtonPress(string buttonMessage){
+        dataLoggerReference.WriteString(buttonMessage+ " button was pressed");
+    }
+
+}
