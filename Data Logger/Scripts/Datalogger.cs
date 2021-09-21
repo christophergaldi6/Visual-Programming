2a2a9afe90cde7d119b63b912f57cba2e85cfefe
Christopher Galdi <42784609+christophergaldi6@users.noreply.github.com>
2021-09-21 02:58:08 -0700

    Data Logger

---
+++ Data Logger/Scripts/Datalogger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Need Access to write text file
+using System.IO;
+//access to Unity edit(Database)
+using UnityEditor;
+//This calls to start the file, print out the time, date, start, and stop times, and then shutsdown.
+public class Datalogger : MonoBehaviour
+{
+    private string fileName = "testLog"; 
+    private string path = "Assets/Logs/";
+    private string filePath;
+
+    // Use this for initialization
+    void Start() 
+    {
+        WriteString("Startup");
+
+    }
+
+    // Update is called once per frame
+    public void WriteString(string message)
+    {
+        //Creates the path, names it, and creates a text file
+        filePath = path + fileName + ".txt";
+
+        StreamWriter writer = new StreamWriter(filePath, true);
+        writer.WriteLine(message); 
+        writer.WriteLine(System.DateTime.Now); //
+        writer.WriteLine("///////////////////////"); //Seperates the time and data output
+
+        writer.Close();
+
+        AssetDatabase.ImportAsset(filePath);
+        Resources.Load(filePath);
+    }
+
+   void OnApplicationQuit(){ //Says shutdown when it is exited. 
+        WriteString("Shutdown");
+    }
+}
---
+++ Data Logger/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour 
+{
+    // Create a variable to link our existing DataLogger.cs in the scene
+    private Datalogger dataLoggerReference;
+    // Use this for initialization
+    // Update is called once per frame
+    void Start()
+    {
+        dataLoggerReference = GameObject.FindGameObjectWithTag("DataLogger").GetComponent<Datalogger>();
+    }
+    void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            dataLoggerReference.WriteString("spacebar was pressed");
+        }
+	}
+
+public void ButtonPress(string buttonMessage)
+{
+    dataLoggerReference.WriteString(buttonMessage + " button was pressed");
+}
+
+}
